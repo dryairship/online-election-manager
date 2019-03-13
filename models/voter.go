f7c1e446f59f232b2a01a9cfbf7eabccde82ea41
Priydarshi Singh <dryairship@gmail.com>
2019-03-14 00:12:18 +0530

    Added Voter model

---
+++ models/voter.go
@@ -0,0 +1,13 @@
+package models
+
+type (
+    Voter struct {
+        Roll        string  `json:"roll"`
+        Name        string  `json:"name"`
+        Email       string  `json:"email"`
+        Password    string  `json:"password"`
+        AuthCode    string  `json:"authcode"`
+        BallotID    string  `json:"ballotid"`
+        Voted       bool    `json:"voted"`
+    }
+) 
