9bc45a2a67308e41f2548ad6c21bd2bcb80c7115
Priydarshi Singh <dryairship@gmail.com>
2019-03-16 20:17:59 +0530

    Added function to check if verification mail should be sent to a student

---
+++ controllers/userTasks.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+    "github.com/dryairship/oem/db"
+)
+
+var ElectionDb db.ElectionDatabase
+
+type UserError struct {
+    reason string
+}
+
+func (err *UserError) Error() string {
+    return err.reason
+}
+
+func CanMailBeSentToStudent(roll string) (bool, error) {
+    voter, err := ElectionDb.FindVoter(roll)
+    if err != nil {
+        return true, nil
+    } else {
+        if voter.AuthCode == "" {
+            return false, &UserError{"Student has already registered."}
+        } else {
+            return false, &UserError{"Verification mail has already been sent to this student."}
+        }
+    }
+}
+
