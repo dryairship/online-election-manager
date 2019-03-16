44809576bc21e807ac18e825b3ae04a7e8580d0b
Priydarshi Singh <dryairship@gmail.com>
2019-03-17 03:37:03 +0530

    Updated router to handle login requests

--- router/router.go
+++ router/router.go
@@ -9,15 +9,11 @@ import (
 
 func SetUpRoutes(r *gin.Engine) {
     
-    session := r.Group("/session")
-    {
-        session.POST("/login", controllers.SessionLogin)
-    }
-    
     users := r.Group("/users")
     {
         users.GET("/mail/:roll", controllers.SendMailToStudent)
         users.POST("/register", controllers.RegisterNewVoter)
+        users.POST("/login", controllers.CheckUserLogin)
     }
     
     r.Use(static.Serve("/",static.LocalFile(config.AssetsPath,true)))
