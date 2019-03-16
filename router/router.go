92622a1ca2c53401d03e8b169116afb5ce758b9b
Priydarshi Singh <dryairship@gmail.com>
2019-03-16 20:35:55 +0530

    Added code to route API requests

---
+++ router/router.go
@@ -0,0 +1,23 @@
+package router
+
+import (
+    "github.com/dryairship/online-election-manager/controllers"
+    "github.com/dryairship/online-election-manager/config"
+    "github.com/gin-gonic/contrib/static"
+    "github.com/gin-gonic/gin"
+)
+
+func SetUpRoutes(r *gin.Engine) {
+    
+    session := r.Group("/session")
+    {
+        session.POST("/login", controllers.SessionLogin)
+    }
+    
+    users := r.Group("/users")
+    {
+        users.GET("/mail/:roll", controllers.SendMailToStudent)
+    }
+    
+    r.Use(static.Serve("/",static.LocalFile(config.AssetsPath,false)))
+}
