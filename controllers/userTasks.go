50478285ed4bb7dcd16cad552574b149fdfd1b5f
Priydarshi Singh <dryairship@gmail.com>
2019-03-17 01:27:50 +0530

    Added API to handle New Voter Registrations

--- controllers/userTasks.go
+++ controllers/userTasks.go
@@ -30,6 +30,43 @@ func CanMailBeSentToStudent(roll string) (bool, error) {
     }
 }
 
+func RegisterNewVoter(c *gin.Context){
+    roll := c.PostForm("roll")
+    passHash := c.PostForm("pass")
+    authCode := c.PostForm("auth")
+    
+    voter, err := ElectionDb.FindVoter(roll)
+    if err != nil {
+        c.String(http.StatusForbidden, "You need to get a verification<br>mail before you register.")
+        return
+    }
+    
+    if voter.AuthCode == "" {
+        c.String(http.StatusForbidden, "Student has already registered.")
+        return
+    }
+    
+    if voter.AuthCode != authCode {
+        c.String(http.StatusBadRequest, "Wrong authentication code.")
+        return
+    }
+    
+    if passHash == "" {
+        c.String(http.StatusBadRequest, "Empty password is not allowed.")
+        return
+    }
+    
+    voter.Password = passHash
+    voter.AuthCode = ""
+    
+    err = ElectionDb.UpdateVoter(roll,&voter)
+    if err != nil {
+        c.String(http.StatusInternalServerError, "Database Error")
+    }else{
+        c.String(http.StatusAccepted, "Voter successfully registered.")
+    }
+}
+
 func SendMailToStudent(c *gin.Context) {
     roll := c.Param("roll")
     _, err := CanMailBeSentToStudent(roll)
