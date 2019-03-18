a9ca8ae8e46abd74eba2e12e3e8c1c528b9bb071
Priydarshi Singh <dryairship@gmail.com>
2019-03-18 13:45:09 +0530

    Added session utility to manage session data

---
+++ utils/session.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+    "github.com/gin-contrib/sessions"
+    "github.com/gin-gonic/gin"
+    "errors"
+)
+
+func StartSession(c *gin.Context) {
+    session := sessions.Default(c)
+    
+    if session.Get("UserType") != nil {
+        session.Clear()
+        session.Save()
+    }
+    
+    roll := c.PostForm("roll")
+    session.Set("UserType","Voter")
+    session.Set("ID",roll)
+    session.Save()
+}
+
+func EndSession(c *gin.Context) {
+    session := sessions.Default(c)
+    session.Clear()
+    session.Save()
+}
+
+func GetSessionID(c *gin.Context) (string, error) {
+    id := sessions.Default(c).Get("ID")
+    if id != nil {
+        return id.(string), nil
+    }
+    return "", errors.New("Unauthenticated User")
+}
