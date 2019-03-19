0c1fcd2d87f0335e3bb0c947718b021820edfa65
Priydarshi Singh <dryairship@gmail.com>
2019-03-19 13:01:54 +0530

    Added SimplifiedCandidate model to represent publicly available data for candidates

--- models/candidate.go
+++ models/candidate.go
@@ -5,10 +5,28 @@ type (
         Roll        string  `json:"roll"`
         Name        string  `json:"name"`
         Email       string  `json:"email"`
+        Username    string  `json:"username"`
         Password    string  `json:"password"`
         AuthCode    string  `json:"authcode"`
+        PostID      string  `json:"postid"`
         Manifesto   string  `json:"manifesto"`
         PublicKey   string  `json:"publickey"`
         PrivateKey  string  `json:"privatekey"`
     }
-) 
+    
+    SimplifiedCandidate struct {
+        Roll        string
+        Name        string
+        PublicKey   string
+        Manifesto   string
+    }
+)
+
+func (candidate Candidate) Simplify() SimplifiedCandidate {
+    return SimplifiedCandidate {
+        Roll:       candidate.Roll,
+        Name:       candidate.Name,
+        PublicKey:  candidate.PublicKey,
+        Manifesto:  candidate.Manifesto,
+    }
+}
