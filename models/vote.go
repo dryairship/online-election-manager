292aec4f038ffab9e3537c9a639c1797807be887
Priydarshi Singh <dryairship@gmail.com>
2019-03-23 22:25:28 +0530

    Added ReceivedVote model for receiving votes from voter

--- models/vote.go
+++ models/vote.go
@@ -10,5 +10,25 @@ type (
         PostID          string
         BallotString    string
     }
+    
+    ReceivedVote struct {
+        PostID          int     `json:"PostID"`
+        BallotString    string  `json:"BallotString"`
+        VoteData        string  `json:"VoteData"`
+    }
 )
+
+func (receivedVote ReceivedVote) GetVote() Vote {
+    return Vote {
+        PostID: string(receivedVote.PostID),
+        Data:   receivedVote.VoteData,
+    }
+}
+
+func (receivedVote ReceivedVote) GetBallotID() BallotID {
+    return BallotID {
+        PostID:         string(receivedVote.PostID),
+        BallotString:   receivedVote.BallotString,
+    }
+}
 
