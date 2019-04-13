0bec1bd502903a461393fe8aeaa4c54b110487cb
Priydarshi Singh <dryairship@gmail.com>
2019-04-14 05:19:32 +0530

    Reduced the length of authentication code

--- utils/random.go
+++ utils/random.go
@@ -12,14 +12,16 @@ func InitializeRandomSeed() {
 
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+// Function to return a random string to be used as authentication code.
 func GetRandomAuthCode() string {
-    b := make([]byte, 32)
+    b := make([]byte, 6)
     for i := range b {
         b[i] = letters[rand.Int63()%int64(len(letters))]
     }
     return string(b)
 }
 
+// Function to get a random time delay after which the vote will be added to the database.
 func GetRandomTimeDelay() time.Duration {
     return time.Duration(rand.Intn(config.MaxTimeDelay)) * time.Second
 }
