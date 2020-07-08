9335f517313cff0d0372af089fc72bfd97e25347
Priydarshi Singh <dryairship@gmail.com>
2020-07-09 04:09:34 +0530

    Add function to export ballot IDs

---
+++ utils/ballot-ids.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/dryairship/online-election-manager/config"
+	"github.com/dryairship/online-election-manager/models"
+)
+
+func ExportBallotIdsToFile(ballotIds []models.UsedBallotID, fileName string) error {
+	path := fmt.Sprintf("%s/ballotids/%s", config.AssetsPath, fileName)
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+
+	for _, ballotId := range ballotIds {
+		_, err = w.WriteString(fmt.Sprintf("%s - %s - %s", ballotId.BallotString, ballotId.Roll, ballotId.Name))
+		if err != nil {
+			return err
+		}
+	}
+
+	w.Flush()
+
+	return nil
+}
